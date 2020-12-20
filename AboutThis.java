9a0d0eab0bf0ca87dfe3abeec9e4c5d9896d0998
Vlad Smirnykh <vlad.ainur@gmail.com>
2020-12-20 11:50:08 +0300

    Add new files

---
+++ AboutThis.java
@@ -0,0 +1,17 @@
+public class AboutThis {
+
+    class Human {
+        String name;
+        int age;
+
+    public void setName(String myName) {
+        name = myName;
+    }
+
+    public void setAge(int myAge) {
+        age = myAge;
+    }
+
+    }
+
+}
